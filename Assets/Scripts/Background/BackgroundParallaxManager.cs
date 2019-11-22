095ba87b4fd3b5fddfee474adec1a8270550711d
duchaussoy <laurent.duchaussoy59@gmail.com>
2019-11-23 00:38:41 +0100

    feat(background): add sky star
    feat(background-start): add rotation
    
    
    feat

---
+++ Assets/Scripts/Background/BackgroundParallaxManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundParallaxManager : MonoBehaviour {
+
+    //public float speed;
+    private GameObject player;
+    private float oldPosY;
+    private float oldPosX;
+    private static float x;
+    private static float y;
+    private Rigidbody2D rg2d;
+    public static BackgroundParallaxManager instance;
+    //Vector2 newPos;
+
+    // TO DO voir a remplacer le player par transform de la camera !!
+
+    void Start() {
+        instance = this;
+        WorldManager.SetPlayer += SetPlayer;
+    }
+
+    private void OnDestroy() {
+        WorldManager.SetPlayer -= SetPlayer;
+    }
+    private void SetPlayer(GameObject player) {
+        this.player = player;
+        rg2d = player.GetComponent<Rigidbody2D>();
+        oldPosY = player.transform.position.y;
+        oldPosX = player.transform.position.x;
+    }
+
+    public static Vector3 GetNewVector2(float speed, bool moveAxeY, Transform t, bool invertAxeY, float ySpeed) {
+        var newY = moveAxeY ? t.position.y - y : t.position.y;
+        if (invertAxeY) {
+            newY = moveAxeY ? t.position.y - (y * ySpeed) : t.position.y;
+        }
+        return new Vector3(t.position.x + x * speed, newY, t.position.z);
+    }
+
+    public void Update() {
+        if (!player)
+            return;
+        x = 0f;
+        y = 0f;
+        if (rg2d.velocity.y != 0) {
+            float newDist = player.transform.position.y - oldPosY;
+            y = newDist;
+            oldPosY = player.transform.position.y;
+        }
+        bool ifEqualX = Mathf.Approximately(player.transform.position.x, oldPosX);
+        if (!ifEqualX) {
+            float newDist = player.transform.position.x - oldPosX;
+            x = -newDist;
+            oldPosX = player.transform.position.x;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Background/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    private Vector2 velocity;
+
+    public float smoothTimeY;
+    public float smoothTimeX;
+    public GameObject player;
+
+    void Start() {
+        WorldManager.SetPlayer += SetPlayer;
+    }
+
+    private void OnDestroy() {
+        WorldManager.SetPlayer -= SetPlayer;
+    }
+
+    private void SetPlayer(GameObject player) {
+        this.player = player;
+    }
+
+    void FixedUpdate() {
+        if (!player)
+            return;
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y + 3, ref velocity.y, smoothTimeY);
+        transform.position = new Vector3(posX, posY, 0);
+    }
+
+}
