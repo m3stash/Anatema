e517c73a1b148fe82aa6c667eed596eaaba4ace7
duchaussoy <laurent.duchaussoy59@gmail.com>
2019-11-27 13:51:25 +0100

    feat(clouds): add procedural generation for cloud
    in progress

---
+++ Assets/Scripts/Background/InfiniteMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteMove : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector3.right * this.speed * Time.deltaTime);
+    }
+
+    public void SetSpeed(float value) {
+        this.speed = value;
+    }
+}
