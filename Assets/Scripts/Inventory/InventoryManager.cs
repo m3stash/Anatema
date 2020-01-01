3d0ac91fceaabebfd9a04e0e4972b885b44c6908
rvanderaspoilden <rodolphevanderaspoilden@gmail.com>
2020-01-01 18:18:25 +0100

    WIP

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -9,6 +9,10 @@ public class InventoryManager : MonoBehaviour
     [SerializeField] private Transform inventorySlotContainer;
     [SerializeField] private GameObject slotItemPrefab;
 
+    [Header("Inventory slots")]
+    [SerializeField] private InventoryItemData[] itemDatas;
+    private int inventorySize = 16;
+
     private List<InventoryItem> items;
     private InventoryCell[] cells;
 
@@ -28,6 +32,7 @@ public class InventoryManager : MonoBehaviour
 
     private void Start()
     {
+        this.itemDatas = new InventoryItemData[this.inventorySize];
         this.items = new List<InventoryItem>();
         this.cells = this.inventorySlotContainer.GetComponentsInChildren<InventoryCell>();
 
@@ -44,70 +49,134 @@ public class InventoryManager : MonoBehaviour
     {
         this.inventoryCanvas.SetActive(!this.inventoryCanvas.activeSelf);
     }
-      
+
+    //public bool AddItem(Item item)
+    //{
+    //    // If item is stackable try to find if we can stack it
+    //    if (item.GetConfig().IsStackable())
+    //    {
+    //        InventoryItem inventoryItem = this.GetExistingInventoryItem(item);
+
+    //        // If same item found in inventory and can be stacked so stack it
+    //        if (inventoryItem)
+    //        {
+    //            inventoryItem.Stack(item.GetStacks());
+    //            return true;
+    //        }
+    //    }
+
+    //    // If item isn't stackable or it couldn't be stacked so try to add it
+    //    InventoryCell cell = this.GetEmptyCell();
+
+    //    if (cell)
+    //    {
+    //        // Create new inventory item in this cell and setup it
+    //        GameObject obj = Instantiate(this.slotItemPrefab, cell.transform);
+    //        InventoryItem inventoryItem = obj.GetComponent<InventoryItem>();
+
+    //        inventoryItem.Setup(item);
+
+    //        this.items.Add(inventoryItem);
+
+    //        // Refresh cell to update inventory item reference
+    //        cell.Refresh();
+
+    //        return true;
+    //    }
+
+
+    //    Debug.Log("No cell found to add item");
+    //    return false;
+    //}
+
+    //private InventoryCell GetEmptyCell()
+    //{
+    //    foreach (InventoryCell cell in this.cells)
+    //    {
+    //        // If a cell haven't inventory item reference, it's free cell
+    //        if (!cell.GetInventoryItem())
+    //        {
+    //            return cell;
+    //        }
+    //    }
+
+    //    return null;
+    //}
+
+    //private InventoryItem GetExistingInventoryItem(Item item)
+    //{
+    //    foreach (InventoryItem inventoryItem in this.items)
+    //    {
+    //        bool itemFoundById = item.GetConfig().GetId().Equals(inventoryItem.GetItem().GetConfig().GetId());
+
+    //        // Check if its same item and addition of stacks is less than stackLimit
+    //        if (itemFoundById && inventoryItem.GetItem().CanStack(item.GetStacks()))
+    //        {
+    //            return inventoryItem;
+    //        }
+    //    }
+
+    //    return null;
+    //}
+
     public bool AddItem(Item item)
     {
         // If item is stackable try to find if we can stack it
         if (item.GetConfig().IsStackable())
         {
-            InventoryItem inventoryItem = this.GetExistingInventoryItem(item);
+            InventoryItemData inventoryItemData = this.GetExistingInventoryItem(item);
 
             // If same item found in inventory and can be stacked so stack it
-            if (inventoryItem)
+            if (inventoryItemData != null)
             {
-                inventoryItem.Stack(item.GetStacks());
+                inventoryItemData.AddStacks(item.GetStacks());
                 return true;
             }
         }
 
         // If item isn't stackable or it couldn't be stacked so try to add it
-        InventoryCell cell = this.GetEmptyCell();
+        int freeSlotIdx = this.GetEmptySlotIdx();
 
-        if (cell)
+        if (freeSlotIdx != -1)
         {
             // Create new inventory item in this cell and setup it
-            GameObject obj = Instantiate(this.slotItemPrefab, cell.transform);
-            InventoryItem inventoryItem = obj.GetComponent<InventoryItem>();
-
-            inventoryItem.Setup(item);
-
-            this.items.Add(inventoryItem);
-
-            // Refresh cell to update inventory item reference
-            cell.Refresh();
+            this.itemDatas[freeSlotIdx] = new InventoryItemData(item.GetConfig(), item.GetStacks(), 100);
 
             return true;
         }
 
 
-        Debug.Log("No cell found to add item");
+        Debug.Log("No slot found to add item");
         return false;
     }
 
-    private InventoryCell GetEmptyCell()
+
+    private int GetEmptySlotIdx()
     {
-        foreach (InventoryCell cell in this.cells)
+        for (int i = 0; i < this.itemDatas.Length; i++)
         {
-            // If a cell haven't inventory item reference, it's free cell
-            if (!cell.GetInventoryItem())
+            if (this.itemDatas[i].GetConfig() == null)
             {
-                return cell;
+                return i;
             }
         }
 
-        return null;
+        return -1;
     }
 
-    private InventoryItem GetExistingInventoryItem(Item item)
+    private InventoryItemData GetExistingInventoryItem(Item item)
     {
-        foreach (InventoryItem inventoryItem in this.items)
+        for(int i = 0; i < this.itemDatas.Length; i++)
         {
-            bool itemFoundById = item.GetConfig().GetId().Equals(inventoryItem.GetItem().GetConfig().GetId());
-
-            // Check if its same item and addition of stacks is less than stackLimit
-            if (itemFoundById && inventoryItem.GetItem().CanStack(item.GetStacks()))
+            if(this.itemDatas[i].GetConfig() != null)
             {
-                return inventoryItem;
+                bool itemFoundById = item.GetConfig().GetId().Equals(this.itemDatas[i].GetConfig().GetId());
+
+                // Check if its same item and addition of stacks is less than stackLimit
+                if (itemFoundById && this.itemDatas[i].CanStack(item.GetStacks()))
+                {
+                    return this.itemDatas[i];
+                }
             }
         }
 
