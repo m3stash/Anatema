8e5116861c333bcebde8ee1f2be84301b37c3533
duchaussoy <laurent.duchaussoy59@gmail.com>
2020-03-11 23:18:34 +0100

    feat(Menu): add main menu

---
+++ Assets/Scripts/Controls/InputMainMenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputMainMenuManager : MonoBehaviour {
+
+    public static InputMainMenuManager instance;
+    public MainMenuControls mainMenuControls;
+
+    private void Awake() {
+        instance = this;
+        this.mainMenuControls = new MainMenuControls();
+    }
+
+    private void Start() {
+       
+    }
+
+    private void OnDestroy() {
+        
+    }
+}
---
+++ Assets/Scripts/MainMenu/MainMenuManagement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManagement: MonoBehaviour {
+
+    public Canvas canvas;
+
+    public void NewGame() {
+        SceneManager.LoadScene("Demo", LoadSceneMode.Single);
+    }
+
+    public void Continue() {
+        
+    }
+
+    private void Start() {
+        // canvas = GameObject.Find("Main_UI").GetComponent<Canvas>();
+    }
+
+}
